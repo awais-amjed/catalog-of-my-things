3bc700e3f5ac074396e59b2c6e44f0e6cd860257
Awais Amjed <awaisamjed171@gmail.com>
2022-05-23 19:07:09 +0500

    Fix Linter issues

--- item.rb
+++ item.rb
@@ -1,10 +1,12 @@
 class Item
   attr_accessor :publish_date
   attr_reader :id, :genre, :source, :author, :label, :archived
-  def initialize(publish_date,id=nil, archived: false)
-    @id =id|| rand(1...1000)
+
+  def initialize(publish_date, id = nil, archived: false)
+    @id = id || rand(1...1000)
     @publish_date = publish_date
     @archived = archived
+
     @genre = []
     @source = []
     @author = []
