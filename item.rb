cd8b0da00a7d28541af33a0a848a7040d700e2aa
Awais Amjed <awaisamjed171@gmail.com>
2022-05-23 18:18:16 +0500

    Add Item class and attributes

---
+++ item.rb
@@ -0,0 +1,31 @@
+class Item
+  attr_accessor :id, :publish_date, :archived
+  attr_reader :genre, :source, :author, :label
+
+  def initialize(id, publish_date, archived: false)
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+
+    @genre = []
+    @source = []
+    @author = []
+    @label = []
+  end
+
+  def add_genre(genre)
+    @genre << genre
+  end
+
+  def add_source(source)
+    @source << source
+  end
+
+  def add_author(author)
+    @author << author
+  end
+
+  def add_label(label)
+    @label << label
+  end
+end
