e7eb6ec574042b04f344a0994d9eb00899ede1a2
Awais Amjed <awaisamjed171@gmail.com>
2022-05-25 22:15:38 +0500

    Fix tests related to Music Album and Genre class

--- music_album.rb
+++ music_album.rb
@@ -5,7 +5,7 @@ class MusicAlbum < Item
   attr_accessor :on_spotify
 
   def initialize(publish_date, id = nil, on_spotify: false, archived: false)
-    super(publish_date, id, archived: archived)
+    super(publish_date: publish_date, id: id, archived: archived)
 
     @on_spotify = on_spotify
   end
